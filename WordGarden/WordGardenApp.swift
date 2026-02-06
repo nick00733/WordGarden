deedf8446b20d412f5fcc8a7688de4712cd49ca8
Nick Nedjat <nick.nedjat@icloud.com>
2026-02-06 12:49:07 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Nick Nedjat on 06.02.26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
