d41d6243881a98d98b089bd46bc209f23458b9e1
Nick Nedjat <nick.nedjat@icloud.com>
2026-02-09 13:12:38 -0500

    2.4

--- WordGarden/ContentView.swift
+++ WordGarden/ContentView.swift
@@ -1,21 +1,104 @@
-//
-//  ContentView.swift
-//  WordGarden
-//
-//  Created by Nick Nedjat on 06.02.26.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wordsGuessed = 0
+    @State private var wordsMissed = 0
+    @State private var gameStatusMessage = "How Many Guesses to Uncover the Hidden Word?"
+    @State private var currentWordIndex = 0   // index in wordsToGuess
+    @State private var wordToGuess = ""
+    @State private var revealedWord = ""
+    @State private var guessedLetter = ""
+    @State private var imageName = "flower8"
+    @State private var playAgainHidden = true
+    @FocusState private var textFieldIsFocused: Bool
+    private let wordsToGuess = ["SWIFT", "DOG", "CAT"] // All Caps
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Words Guessed: \(wordsGuessed)")
+                    Text("Words Missed: \(wordsMissed)")
+                }
+
+                Spacer()
+
+                VStack(alignment: .trailing) {
+                    Text("Words to Guess: \(wordsToGuess.count - (wordsGuessed + wordsMissed))")
+                    Text("Words in Game: \(wordsToGuess.count)")
+                }
+            }
+
+            Spacer()
+
+            Text(gameStatusMessage)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .padding()
+
+            // TODO: Switch to wordsToGuess[currentWord]
+            let revealedWord = "SWIFT"
+
+            var mystWord: String {
+                var result = ""
+                for _ in revealedWord {
+                    result += "_ "
+                }
+                result.removeLast()
+                return result
+            }
+
+            Text(mystWord)
+                .font(.title)
+
+            if playAgainHidden {
+                HStack {
+                    TextField("", text: $guessedLetter)
+                        .textFieldStyle(.roundedBorder)
+                        .frame(width: 30)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.gray, lineWidth: 2)
+                        }
+                        .keyboardType(.asciiCapable)
+                        .submitLabel(.done)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.characters)
+                        .onChange(of: guessedLetter) {
+                            guessedLetter =
+                                guessedLetter.trimmingCharacters(in: .letters.inverted)
+
+                            guard let lastChar = guessedLetter.last else {
+                                return
+                            }
+
+                            guessedLetter = String(lastChar).uppercased()
+                        }
+                        .focused($textFieldIsFocused)
+                    
+                    Button("Guess a Letter:") {
+                        // TODO: Guess a Letter button action here
+                        textFieldIsFocused = false
+                    }
+                    .buttonStyle(.bordered)
+                    .tint(.mint)
+                    .disabled(guessedLetter.isEmpty)
+                }
+            } else {
+                Button("Another Word?") {
+                    playAgainHidden = true
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.mint)
+            }
+
+            Spacer()
+
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .ignoresSafeArea(edges: .bottom)
     }
 }
 
